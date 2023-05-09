25eb58cdc456df92abc3297debd744611a61d45f
Kitana <kittoft@gmail.com>
2023-05-08 23:42:24 -0700

    running into 405 err with Apollo client: need to fix

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,6 @@
+import { getClient } from "@/apollo-client";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+
 type Props = {
   params: {
     city: string;
@@ -6,7 +9,22 @@ type Props = {
   };
 };
 
-function Weatherpage({ params: { city, lat, long } }: Props) {
+async function Weatherpage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: "true",
+      latitude: lat,
+      longitude: long,
+      timezone: "GMT",
+    }
+  })
+
+  const results: Root = data.myQuery;
+  console.log(results)
+
   return (
     <div>
       Welcome to the weather page {city} {lat} {long}
