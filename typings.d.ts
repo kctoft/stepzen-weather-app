99b4dcef057946a4e160c95785567537e264a972
Kitana <kittoft@gmail.com>
2023-05-08 23:18:31 -0700

    created 1:1 typings

--- typings.d.ts
+++ typings.d.ts
@@ -7,16 +7,16 @@ interface CurrentWeather {
   windspeed: number;
 }
 interface Daily {
-  apparent_temperature_max: string;
-  apparent_temperature_min: string;
-  sunrise: string;
-  sunset: string;
-  temperature_2m_max: string;
-  temperature_2m_min: string;
-  time: string;
-  uv_index_clear_sky_max: string;
-  uv_index_max: string;
-  weathercode: number;
+  apparent_temperature_max: [number];
+  apparent_temperature_min: [number];
+  sunrise: [string];
+  sunset: [string];
+  temperature_2m_max: [number];
+  temperature_2m_min: [number];
+  time: [string];
+  uv_index_clear_sky_max: [number];
+  uv_index_max: [number];
+  weathercode: [number];
 }
 interface DailyUnits {
   apparent_temperature_max: string;
@@ -72,5 +72,5 @@ interface Root {
   latitude: number;
   longitude: number;
   timezone: string;
-  windspeed_unit: string;
+  utc_offset_seconds: number;
 }
